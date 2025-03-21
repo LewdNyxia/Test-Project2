6bb94c8d28f0f54a72e6a4dc34f37d17d3c26a34
Alstor <zamorak789@gmail.com>
2025-03-21 18:49:50 -0500

    removed dialogic

--- main.gd
+++ main.gd
@@ -1,7 +1,29 @@
 extends Control
 
 
+@onready var room_manager = $RoomManager
+@onready var playfield = $"/root/Playfield"
+@onready var label = $MarginContainer/BoxContainer/VBoxContainer/HBoxContainer/MarginContainer2/MarginContainer/MainTextPort
+
+
+
 func _on_check_button_toggled(toggled_on: bool) -> void:
 	var window = toggled_on
 	if window == true: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN) 
 	elif window == false: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED) 
+var current_room = null
+func initialize(starting_room):
+	current_room = starting_room
+
+func updateText(text):
+	label.text = text
+
+
+##func _ready() -> void:
+##	initalize(room_manager.get_child(0))
+##	updateText("AAAAA")
+
+
+
+	
+	
---
+++ room.gd
@@ -0,0 +1,4 @@
+extends PanelContainer
+
+@export var room_name = "House"
+@export var room_description = "A house"
