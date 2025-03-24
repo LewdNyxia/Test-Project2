f417cb64e6f0677a52d8873e69b2c773c4acb7ed
Alstor <zamorak789@gmail.com>
2025-03-24 00:50:22 -0500

    begin room code

--- main.gd
+++ main.gd
@@ -5,6 +5,12 @@ extends Control
 @onready var playfield = $"/root/Playfield"
 @onready var label = $MarginContainer/BoxContainer/VBoxContainer/HBoxContainer/MarginContainer2/MarginContainer/MainTextPort
 
+var HID = 0
+var VID = 0
+var N = 0
+var S = 0
+var W = 0
+var E = 0
 
 
 func _on_check_button_toggled(toggled_on: bool) -> void:
@@ -18,12 +24,28 @@ func initialize(starting_room):
 func updateText(text):
 	label.text = text
 
+func RoomData(IDH, IDV, exitN, exitS, exitE, exitW, name, description):
+	updateText("You are in" +name +description)
+	HID = IDH
+	VID = IDV
+	N = exitN
+	S = exitS
+	W = exitW
+	E = exitE
 
-##func _ready() -> void:
-##	initalize(room_manager.get_child(0))
-##	updateText("AAAAA")
+
+func _on_north_pressed() -> void:
+	if N == 1
+		
 
 
+func _on_west_pressed() -> void:
+	pass # Replace with function body.
 
-	
-	
+
+func _on_south_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_east_pressed() -> void:
+	pass # Replace with function body.
--- room.gd
+++ room.gd
@@ -1,4 +1,13 @@
 extends PanelContainer
-
+@onready var Main = get_node("/root/playfield/UI")
 @export var room_name = "House"
 @export var room_description = "A house"
+@export var room_idH = 0
+@export var room_idV = 0
+@export var exitN = 0
+@export var exitS = 0
+@export var exitE = 0
+@export var exitW = 0
+
+func _process(delta: float) -> void:
+	Main.RoomData(room_idH, room_idV, exitN, exitS, exitE, exitW, room_name, room_description)
