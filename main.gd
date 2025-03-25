2880f5a5e71f1218f118baa05169b4e49c799d8a
Alstor <zamorak789@gmail.com>
2025-03-25 08:05:02 -0500

    rooms with exits!!

--- main.gd
+++ main.gd
@@ -4,48 +4,48 @@ extends Control
 @onready var room_manager = $RoomManager
 @onready var playfield = $"/root/Playfield"
 @onready var label = $MarginContainer/BoxContainer/VBoxContainer/HBoxContainer/MarginContainer2/MarginContainer/MainTextPort
-
+@onready var House = room_manager.get_child(0)
 var HID = 0
 var VID = 0
 var N = 0
 var S = 0
 var W = 0
 var E = 0
-
+func _ready() -> void:
+	initialize(House)
 
 func _on_check_button_toggled(toggled_on: bool) -> void:
 	var window = toggled_on
 	if window == true: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN) 
 	elif window == false: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED) 
+
 var current_room = null
 func initialize(starting_room):
-	current_room = starting_room
+	changeRoom(starting_room)
+
+func changeRoom(new_room: GameRoom):
+	current_room = new_room
+	var exit_string = PackedStringArray(new_room.exits.keys())
+	var strings = "You are now in: " +new_room.room_name + ". It is " +new_room.room_description + \
+	"\nYou can go " + ",".join(exit_string)
+	updateText(strings)
+	pass
 
 func updateText(text):
 	label.text = text
 
-func RoomData(IDH, IDV, exitN, exitS, exitE, exitW, name, description):
-	updateText("You are in" +name +description)
-	HID = IDH
-	VID = IDV
-	N = exitN
-	S = exitS
-	W = exitW
-	E = exitE
-
-
 func _on_north_pressed() -> void:
-	if N == 1
-		
-
+	if current_room.exits.has("north"):
+		changeRoom(current_room.exits["north"])
 
 func _on_west_pressed() -> void:
-	pass # Replace with function body.
-
+	if current_room.exits.has("west"):
+		changeRoom(current_room.exits["west"])
 
 func _on_south_pressed() -> void:
-	pass # Replace with function body.
-
+	if current_room.exits.has("south"):
+		changeRoom(current_room.exits["south"])
 
 func _on_east_pressed() -> void:
-	pass # Replace with function body.
+	if current_room.exits.has("east"):
+		changeRoom(current_room.exits["east"])
--- room.gd
+++ room.gd
@@ -1,13 +1,32 @@
 extends PanelContainer
+class_name GameRoom
+
 @onready var Main = get_node("/root/playfield/UI")
 @export var room_name = "House"
 @export var room_description = "A house"
 @export var room_idH = 0
 @export var room_idV = 0
-@export var exitN = 0
-@export var exitS = 0
-@export var exitE = 0
-@export var exitW = 0
+
+@onready var exits: Dictionary = {}
+
+func connectExit(dir: String, room: GameRoom):
+	match dir:
+		"west":
+			exits[dir] = room
+			room.exits["east"] = self
+		"east":
+			exits[dir] = room
+			room.exits["west"] = self
+		"north":
+			exits[dir] = room
+			room.exits["south"] = self
+		"south":
+			exits[dir] = room
+			room.exits["north"] = self
+	pass
 
 func _process(delta: float) -> void:
-	Main.RoomData(room_idH, room_idV, exitN, exitS, exitE, exitW, room_name, room_description)
+	pass
+	
+func _ready() -> void:
+	pass
